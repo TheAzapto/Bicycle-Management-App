dd3dfbb02cb38d90a5c1031ac2c0328a02c18409
AayushDutta <aayush.dutta22@st.niituniversity.in>
2025-04-29 10:39:59 +0530

    modification

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -4,14 +4,22 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "<YOUR_API_KEY>",
-  authDomain: "<YOUR_AUTH_DOMAIN>",
-  projectId: "<YOUR_PROJECT_ID>",
-  storageBucket: "<YOUR_STORAGE_BUCKET>",
-  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
-  appId: "<YOUR_APP_ID>",
+  apiKey: "AIzaSyAEpbBg5r9o6m5Wdp_tF995xMl4KduREfQ",
+  authDomain: "myryde-49810.firebaseapp.com",
+  projectId: "myryde-49810",
+  messagingSenderId: "399067785924",
+  appId: "1:399067785924:web:f5b7f146ae922941e6b791",
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAEpbBg5r9o6m5Wdp_tF995xMl4KduREfQ",
+//   authDomain: "myryde-49810.firebaseapp.com",
+//   projectId: "myryde-49810",
+//   storageBucket: "myryde-49810.firebasestorage.app",
+//   messagingSenderId: "399067785924",
+//   appId: "1:399067785924:web:f5b7f146ae922941e6b791",
+//   measurementId: "G-F5PR31QPQP"
+// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
